3fbe9b59343cd692ce90de9e45ee4ea6d87569d9
george <gkephart@cnm.edu>
2019-06-13 13:44:22 -0600

    coded up example for the students

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,5 +1,25 @@
-const onPageLoad =  () => {
-	 axios.get("./php/apis").then(({data}) => {
-	 	console.log(data)
-	 });
-} ;
+const onPageLoad = () => {
+	axios.get("./php/apis").then(({data}) => {
+		let cards =  createCard(data.data);
+		console.log(cards);
+		let html = document.getElementById("target");
+		html.innerHTML = cards.join("\r\n");
+
+	});
+};
+
+const createCard = reply => reply.map(reply => {
+
+	return (
+		`
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">${reply.title}</h5>
+					<p class="card-text">${reply.body}
+					</p>
+			</div>
+		</div>
+
+	`
+	)
+});
